cb2736e820d5f0a5179ecee8103e5f7ad0acaf17
Vivek Kumar <139310849+thefearlesscoder@users.noreply.github.com>
2025-06-28 17:05:33 +0000

    dericed class implemtation

--- inheritance.cpp
+++ inheritance.cpp
@@ -3,3 +3,31 @@
 #include<iostream>
 using namespace std;
 
+class Human{
+    public:
+    string name;
+    int age;
+    int weight;
+};
+
+// class student: <access modifier> Human
+class Student : public Human{
+    private:
+    int rollNumber;
+    int fees;
+
+    public:
+    void fun(string n, int a, int w){
+        name=n;
+        age = a;
+        weight = w;
+    }
+};
+
+
+int main(){
+    Student a;
+    // a.name = "Rohit"; // child mien bhi public hi treat hoga
+    a.fun("Rohit", 20, 12); // this works fine
+
+}
